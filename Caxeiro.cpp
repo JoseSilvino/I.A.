ba9b2addb23ed6685a9efee7ba4490bfd44f14e9
José Silvino <jssn@ic.ufal.br>
2019-11-25 10:01:38 -0300

    Update Caxeiro.cpp

--- Caxeiro.cpp
+++ Caxeiro.cpp
@@ -106,6 +106,14 @@ void DFS(int start,int path[11],int graph[10][10],int &ind){
 		}
 	}
 }
+int getIndex(MyWay way,int city){
+	int i=0;
+	for(auto x:way.caminho) {
+		if(x==city) return i;
+		i++;
+	}
+	return -1;
+}
 int main() {
 	int matriz[10][10] = {
 	{0, 30, 84, 56, -1, -1, -1, 75, -1, 80},
@@ -118,42 +126,34 @@ int main() {
 	{ 75, -1, 135, 20, 122, -1, -1,0, -1, -1},
 	{ -1,-1,143, -1, 98, 82, 120, -1, 0, -1 },
 	{ 80, 40, 48, -1, 80 ,35, 57, -1, -1,0 } };
-	int comeco;
-	cin >> comeco;
+	int comeco=0;
+	while(comeco<1||comeco>10){
+		cout<<"Digite uma cidade inicial entre 1 e 10\n";
+		cin >> comeco;
+	}
         comeco--;
-	cout<<"Pode demorar alguns minutos , por favor tenha paciencia\n";
-	cout<<"Cidade inicial "<<comeco<<endl;
+	cout<<"Pode demorar alguns segundos , por favor tenha paciencia\n";
+	cout<<"Cidade inicial: C"<<comeco+1<<endl;
 	int a[11] = { 0 };
-	a[0] = comeco;
-	a[10] = comeco;
-	BFS(comeco, a, matriz);
+	a[0] = 0;
+	a[10] = 0;
+	BFS(0, a, matriz);
 	MyWay max(a, matriz);
 	max.distance = INT_MAX;
 	int tmp[11];
 	for (int n = 0; n < 6; n++) {
 		max.setArr(tmp);
 		int mp = max.distance;
-		tryLower(tmp, matriz, max, 0,4);
+		tryLower(tmp, matriz, max, 0,3);
 		if(max.distance==mp) {
 			break;
 		}
 	}
-	int ind=0;
-	DFS(comeco,a,matriz,ind);
-	MyWay max2(a,matriz);
-	max2.distance=INT_MAX;
-	for (int n = 0; n < 6; n++) {
-		max2.setArr(tmp);
-		int mp = max2.distance;
-		tryLower(tmp, matriz, max2, 0,4);
-		if(max2.distance==mp) {
-			break;
-		}
-	}
-	max = min(max,max2);
 	cout <<"\nDistancia minima encontrada :"<< max.distance<<endl;
 	cout<<"Caminho encontrado:\n";
-	for (int i = 0; i < 11; i++) cout <<"C" <<max.caminho[i]+1 << " ";
+	int comeco_index = getIndex(max,comeco);
+	for(int i = comeco_index;i<11;i++) cout<<"C"<<max.caminho[i]+1<<" ";
+	for(int i=1;i<=comeco_index;i++) cout<<"C"<<max.caminho[i]+1<<" ";
 	cout << endl;
 	return 0;
 } 
