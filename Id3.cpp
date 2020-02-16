428b9db7b7e8e3b3c61856b954650f5c0efccd70
José Silvino <jssn@ic.ufal.br>
2020-02-16 11:51:15 -0300

    Add files via upload

--- Id3.cpp
+++ Id3.cpp
@@ -1,10 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 vector < vector < string > > base;
+vector < string >  or_atts;
 template <typename t>
 bool contains(vector<t> vet,t c){
     for(t x : vet) if(x==c) return true;
     return false;
+}
+int indexof(vector<string> v,string s){
+    for(int i=0;i<(int)v.size();i++) if(v[i]==s) return i;
+    return -1;
 }
 vector<string> system_classes (){
     vector<string> classes;
@@ -15,10 +20,34 @@ vector<string> system_classes (){
     }
     return classes;
 }
+int occurrences(string val,int index){
+    int oc=0;
+    for(auto x:base){
+        if(x[index]==val) oc++;
+    }
+    return oc;
+}
 double entropy_system(){
-    
+    vector<string> sc = system_classes();
+    double result=0;
+    for(int i = 0 ; i < (int)sc.size();i++){
+        double divi = (double)occurrences(sc[i],base.size()-1)/(double)base.size();
+        result += (divi)*log2(divi);
+    }
+    return -result;
 }
 double entropy(string att,vector<int> atts){
+    int j = indexof(or_atts,att);
+    vector<string> values;
+    vector<int> freq(atts.size(),0);
+    for(int x:atts){
+        if(!contains(values,base[x][j])){
+            values.push_back(base[x][j]);
+        }
+        freq[indexof(values,base[x][j])]++;
+    }
+    vector <string> sc = system_classes();
+    vector <vector <int> > freqvalues(sc.size(),vector(values.size(),0));
 
 }
 double gain(vector<int>regs,string att){
@@ -35,10 +64,7 @@ string max_gain(vector<int> regs,vector<string> atts){
     }
     return att;
 }
-int indexof(vector<string> v,string s){
-    for(int i=0;i<(int)v.size();i++) if(v[i]==s) return i;
-    return -1;
-}
+
 int max_value(vector<int> v){
     int max_v = 0;
     for(int i=1;i<(int)v.size();i++){
@@ -48,8 +74,7 @@ int max_value(vector<int> v){
 }
 pair<vector<string>,int> get_classes(vector<int> regs){
     vector<string> ret;
-    vector<int> freq;
-    freq.resize(regs.size());
+    vector<int> freq(regs.size(),0);
     for(int i:regs){
         string classe = base[i][((int)base[i].size())-1];
         if(!contains(ret,class)){
@@ -63,8 +88,12 @@ string predominant_class(vector<int> regs){
     pair<vector<string>,int> x = get_classes(regs);
     return x.first[x.second];
 }
-double error_tx(string pc){
-
+double error_tx(string pc,vector<int>regs){
+    double err =0;
+    for(int i:regs){
+        if(base[i][base.size()-1]!=pc) err++;
+    }
+    return err/(double)regs.size();
 }
 class Node{
     public:
@@ -78,14 +107,14 @@ class Node{
         this->classification = classification;
         this->error_tx = error_tx;
     }
-    generate_node(vector<int> registers,vector<string> attributes){
+    void generate_node(vector<int> registers,vector<string> attributes){
         vector<string> classes = get_classes(registers).first;
         if(classes.size()>1){
             if(attributes.size()>0){
-                string attribute = max_entropy(registers,attributes);
+                string attribute = max_gain(registers,attributes);
                 //for values in attribute node = new Node(),node.generate_node(registers(value),attributes - {attribute});
             }else{
-                children.push_back(new Node(predominant_class(registers),error_tx(predominant_class(registers))));
+                children.push_back(new Node(predominant_class(registers),error_tx(predominant_class(registers),registers)));
             }
         }else{
             children.push_back(new Node(classes[0],0.0));
